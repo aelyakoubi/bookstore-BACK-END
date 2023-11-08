001b1301a7ab5add739dddffe59ee7b1d26a3a1f
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2023-11-08 19:38:40 +0100

    Update getUserOrders.js

--- services/users/getUserOrders.js
+++ services/users/getUserOrders.js
@@ -5,21 +5,21 @@ const getUserOrders = async (userId) => {
   const prisma = new PrismaClient();
 
   const userOrders = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-    ///////   zelf toegevoegd  // Add other scalar fields you need
-    select: {
-      id: true,
-      username: true,
-    },
+    where: { id: userId },
     include: {
-      orders: true,
+      Orders: {
+        include: {
+          book: true, // Include book details
+          record: true, // Include record details
+        },
+      },
     },
   });
+
   if (!userOrders) {
     throw new NotFoundError("User", userId);
   }
+
   return userOrders;
 };
 
